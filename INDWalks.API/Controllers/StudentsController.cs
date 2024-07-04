5060d2e8b662be3b44b2f29ebc696bbff5c40b93
Manibabu Chittemsetty <ch.mani9144@gmail.com>
2024-07-05 02:19:46 +0530

    Intitial Commmit

---
+++ INDWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace INDWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] names = new string[] { "Mani", "Saida", "Rafi", "Sai" };
+
+            return Ok(names);
+        }
+    }
+}
